8a7d8a49ca5018ce8f2cdb50152182ff926f34b6
shenbaise9527 <zhouyingan@gmail.com>
2020-12-07 17:19:56 +0800

    update tracer

--- tracing.go
+++ tracing.go
@@ -2,6 +2,7 @@ package tracing
 
 import (
 	"context"
+	"github.com/opentracing/opentracing-go/ext"
 	"io"
 	"net/http"
 
@@ -9,6 +10,24 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
+
+type withHTTPCodeResponse struct {
+	writer http.ResponseWriter
+	code int
+}
+
+func (w *withHTTPCodeResponse) Header() http.Header {
+	return w.writer.Header()
+}
+
+func (w *withHTTPCodeResponse) Write(bytes []byte) (int, error) {
+	return w.writer.Write(bytes)
+}
+
+func (w *withHTTPCodeResponse) WriteHeader(code int) {
+	w.writer.WriteHeader(code)
+	w.code = code
+}
 
 // NewOpenTracer 初始化opentracing,第一个返回值在外部应该调用close方法.
 func NewOpenTracer(tracingURL, serverName, localEndpoint string) (io.Closer, error) {
@@ -27,18 +46,27 @@ func HttpTracing(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tracer := opentracing.GlobalTracer()
 		carrier := opentracing.HTTPHeadersCarrier(r.Header)
-		clientspan, err := tracer.Extract(opentracing.HTTPHeaders, carrier)
+		clientSpan, err := tracer.Extract(opentracing.HTTPHeaders, carrier)
 		var span opentracing.Span
 		if err == nil {
-			span = tracer.StartSpan(r.RequestURI, opentracing.ChildOf(clientspan))
+			span = tracer.StartSpan(r.RequestURI, opentracing.ChildOf(clientSpan))
 		} else {
 			span = tracer.StartSpan(r.RequestURI)
 		}
 
 		defer span.Finish()
+		cw := &withHTTPCodeResponse{writer: w}
 		rc := opentracing.ContextWithSpan(r.Context(), span)
 		r = r.WithContext(rc)
-		next(w, r)
+		defer func() {
+			ext.HTTPMethod.Set(span, r.Method)
+			ext.HTTPStatusCode.Set(span, uint16(cw.code))
+			if cw.code >= http.StatusBadRequest {
+				ext.Error.Set(span, true)
+			}
+		}()
+
+		next(cw, r)
 	}
 }
 
@@ -56,42 +84,55 @@ func OpenTracingServerInterceptor() grpc.UnaryServerInterceptor {
 				carrier.Set(k, v[0])
 			}
 
-			spanctx, err := tracer.Extract(opentracing.TextMap, carrier)
+			spanCtx, err := tracer.Extract(opentracing.TextMap, carrier)
 			if err != nil {
 				span = tracer.StartSpan(info.FullMethod)
 			} else {
-				span = tracer.StartSpan(info.FullMethod, opentracing.ChildOf(spanctx))
+				span = tracer.StartSpan(info.FullMethod, opentracing.ChildOf(spanCtx))
 			}
 		}
 
 		defer span.Finish()
 		ctx = opentracing.ContextWithSpan(ctx, span)
-		return handler(ctx, req)
+		resp, err = handler(ctx, req)
+		if err != nil {
+			ext.LogError(span, err)
+		}
+
+		return
 	}
 }
 
 // OpenTracingClientInterceptor grpc unary clientinterceptor.
 func OpenTracingClientInterceptor() grpc.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-		span:= opentracing.SpanFromContext(ctx)
+		span := opentracing.SpanFromContext(ctx)
+		tracer := opentracing.GlobalTracer()
 		if span != nil {
-			tracer := span.Tracer()
-			clientspan := tracer.StartSpan(method, opentracing.ChildOf(span.Context()))
-			defer clientspan.Finish()
-			carrier := make(opentracing.TextMapCarrier)
-			err := tracer.Inject(clientspan.Context(), opentracing.TextMap, carrier)
-			if err == nil {
-				var pairs []string
-				_ = carrier.ForeachKey(func(key, val string) error {
-					pairs = append(pairs, key, val)
-					return nil
-				})
-
-				ctx = metadata.AppendToOutgoingContext(ctx, pairs...)
-			}
+			span = tracer.StartSpan(method, opentracing.ChildOf(span.Context()))
+		} else {
+			span = tracer.StartSpan(method)
 		}
 
-		return invoker(ctx, method, req, reply, cc, opts...)
+		defer span.Finish()
+		carrier := make(opentracing.TextMapCarrier)
+		err := tracer.Inject(span.Context(), opentracing.TextMap, carrier)
+		if err == nil {
+			var pairs []string
+			_ = carrier.ForeachKey(func(key, val string) error {
+				pairs = append(pairs, key, val)
+				return nil
+			})
+
+			ctx = metadata.AppendToOutgoingContext(ctx, pairs...)
+		}
+
+		err = invoker(ctx, method, req, reply, cc, opts...)
+		if err != nil {
+			ext.LogError(span, err)
+		}
+
+		return err
 	}
 }
 
